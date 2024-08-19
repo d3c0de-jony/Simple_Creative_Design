e5e276235c65be27f551922d8a7a4eaf6d9d42bf
jony <tchsaan@gmail.com>
2024-08-19 23:37:35 +0530

    these are making by me

---
+++ script.js
@@ -0,0 +1,41 @@
+function clickButton() {
+    document.getElementById("output-1").innerHTML = Date();
+    document.getElementById("circle1").style.backgroundColor = "red";
+}
+
+function clickButton2() {
+    document.getElementById("output-2").innerHTML = Date();
+    document.getElementById("circle1").style.backgroundColor = "black";
+    document.getElementById("circle2").style.backgroundColor = "red";
+}
+
+function clickButton3() {
+    document.getElementById("output-3").innerHTML = Date();
+    document.getElementById("circle2").style.backgroundColor = "black";
+    document.getElementById("circle3").style.backgroundColor = "red";
+}
+
+function clickButton4() {
+    document.getElementById("output-4").innerHTML = Date();
+    document.getElementById("circle3").style.backgroundColor = "black";
+    document.getElementById("circle4").style.backgroundColor = "red";
+}
+
+function clickButton5() {
+    document.getElementById("output-5").innerHTML = Date();
+    document.getElementById("circle4").style.backgroundColor = "black";
+    document.getElementById("circle5").style.backgroundColor = "red";
+}
+
+function clickButton6() {
+    document.getElementById("output-6").innerHTML = Date();
+    document.getElementById("circle5").style.backgroundColor = "black";
+    document.getElementById("circle6").style.backgroundColor = "red";
+    
+}
+
+function clickButton7() {
+    document.getElementById("output-7").innerHTML = Date();
+    document.getElementById("circle6").style.backgroundColor = "black";
+    document.getElementById("circle7").style.backgroundColor = "red";
+}
